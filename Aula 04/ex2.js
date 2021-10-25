cd60c112e49a2480d7c9b53f7d08ae0a83d82aa3
Ana Teixeira <89254752+ateixeiradh@users.noreply.github.com>
2021-10-25 20:22:45 -0300

    envio ex2 e ex3 aula ao vivo

---
+++ Aula 04/ex2.js
@@ -0,0 +1,10 @@
+// coloque comentário
+let nome = "Danilo";
+let idade = 30;
+let usuarioAtivo = true;
+
+console.log("NOME", nome)
+console.log(nome);
+console.log("IDADE", idade)
+console.log(idade);
+console.log(usuarioAtivo);
---
+++ Aula 04/ex3.js
@@ -0,0 +1,9 @@
+let numero1 = 1;
+let numero2 = 2;
+
+console.log(numero1+numero2);
+
+console.log(1+1);
+
+var soma = 10+5;
+console.log(soma);
