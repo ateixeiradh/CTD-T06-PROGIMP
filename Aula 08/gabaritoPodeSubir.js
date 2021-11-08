dd5a7d1ea83a1dc9833d6c5efa9f947b6d126692
Ana Teixeira <89254752+ateixeiradh@users.noreply.github.com>
2021-11-08 19:43:37 -0300

    Início Aula 08

---
+++ Aula 08/gabaritoPodeSubir.js
@@ -0,0 +1,38 @@
+// O QUE É CONTROLE DE FLUXO
+// TOMADA DE DECISÃO DO PROGRAMA
+// CONDIÇÕES QUE QUANDO ATENDIDAS OU NÃO 
+// O SEU CÓDIGO TEM UMA AÇÃO
+
+// EXEMPLOS PARA CONTROLE DE FLUXO da AULA 08
+// Switch
+// IF/ELSE
+
+// TODOS TEM UMA CONDIÇÃO
+
+// ESTRUTURA IF-ELSE
+// IF -> (condição)== true -> faz uma ação
+// ELSE -> caso contrário
+// escrita de código TEXTUAL -> SE e SENÃO
+
+
+/***
+ * Em um parque de diversões nos pedem 
+ * um programa para verificar se os passageiros da montanha-russa podem entrar no brinquedo.
+ * 
+ * Crie uma função podeSubir() que receba dois parâmetros: 
+ * altura da pessoa;
+ * se está acompanhada.
+ * 
+ * Deve retornar um valor booleano (TRUE, FALSE) que indique se a pessoa pode subir ou não,
+ *  baseado nas seguintes condições:
+ * A pessoa deve medir mais de 1.40m e menos de 2 metros.
+ * Se a pessoa medir menos de 1.40m, deverá ir acompanhada.
+ * Se a pessoa medir menos de 1.20m, não poderá subir, nem acompanhada.
+*/
+
+/**
+ * 
+ * @param {*} altura 
+ * @param {*} vemAcompanhado 
+ * @returns podeSubir 
+ */
