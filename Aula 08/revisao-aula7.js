2c99c5b089ca18fa709a5e712c0e0daf4aa6c67a
Ana Teixeira <89254752+ateixeiradh@users.noreply.github.com>
2021-11-08 19:27:33 -0300

    Revisao Aula07

---
+++ Aula 08/revisao-aula7.js
@@ -0,0 +1,10 @@
+let x = 5;
+let y = 9;
+
+console.log( x === y ); // comparacao ESTRITA: valor E tipo
+console.log( ! (x === y) ); // comparacao ESTRITA: valor E tipo
+
+console.log( x ); // mostrar x no valor numérico
+console.log( !x ); // negacao do valor numérico sempre falsa
+console.log( !x === y ); // COMPARA o resultado da negacao de x com comparacao ESTRITA de y
+
