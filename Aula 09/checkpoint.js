409bdb0183636a149289d31c9356fb5f77ebc4cc
Ana Teixeira <89254752+ateixeiradh@users.noreply.github.com>
2021-11-09 20:05:07 -0300

    NORTE DO CHECKPOINT

---
+++ Aula 09/checkpoint.js
@@ -0,0 +1,23 @@
+// menu -> escolha e opções
+
+// QUAIS OPÇÕES?
+// PRATOS
+// 1 A 5
+
+// 1 - PIPOCA
+// tempoPadrao = 10s
+// tempoUsuario = x
+// SE tempoUsuario > 2*tempoPadrao = PIPOCA QUEIMOU
+// SE tempoUsuario < tempoPadrao = TEMPO INSUFICIENTE
+// SE tempoUsuario > 3*tempoPadrao = KABUM
+// SENÃO = PRATO PRONTO
+
+// 2 - MACARRÃO
+
+// 3 - CARNE
+
+// 4 - FEIJÃO
+
+// 5 - BRIGADEIRO
+
+// SE PRATO FOR DIFERENTE DOS VALORES DE 1 - 5 = PRATO INEXISTENTE
